5b5313f19cbea8e6c4ab1723a8ceb231b6c49f3f
Michael Wathen <mwathen@cs.ubc.ca>
2016-04-14 17:07:08 -0700

    Updates

--- Project/scr/MHD/MHDsolve.py
+++ Project/scr/MHD/MHDsolve.py
@@ -25,13 +25,41 @@ def Multigrid(A):
     ksp.setOperators(A,A)
     return ksp
 
+def MultigridCG(A):
+    ksp = PETSc.KSP()
+    ksp.create(comm=PETSc.COMM_WORLD)
+    pc = ksp.getPC()
+    ksp.setType('cg')
+    pc.setType('gamg')
+    ksp.setOperators(A,A)
+    return ksp
+
+def JacobiCG(A):
+    ksp = PETSc.KSP()
+    ksp.create(comm=PETSc.COMM_WORLD)
+    pc = ksp.getPC()
+    ksp.setType('cg')
+    pc.setType('jacobi')
+    ksp.setOperators(A,A)
+    return ksp
+
 level = str(3)
 HX = [Scipy2PETSc(loadmat('Mat/'+level+'A0.mat')['Mat/'+level+'A0']),
     [Scipy2PETSc(loadmat('Mat/'+level+'A10.mat')['Mat/'+level+'A10']),
     Scipy2PETSc(loadmat('Mat/'+level+'A11.mat')['Mat/'+level+'A11'])],
     Multigrid(Scipy2PETSc(loadmat('Mat/'+level+'A2.mat')['Mat/'+level+'A2'])),
-    Scipy2PETSc(loadmat('Mat/'+level+'A3.mat')['Mat/'+level+'A3']),
-    Scipy2PETSc(loadmat('Mat/'+level+'A4.mat')['Mat/'+level+'A4']),1,
+    Multigrid(Scipy2PETSc(loadmat('Mat/'+level+'A3.mat')['Mat/'+level+'A3'])),
+    MultigridCG(Scipy2PETSc(loadmat('Mat/'+level+'A4.mat')['Mat/'+level+'A4'])),
+    1,
     Scipy2PETSc(loadmat('Mat/'+level+'A6.mat')['Mat/'+level+'A6'])]
 
-print HX
+Fluid = [Multigrid(Scipy2PETSc(loadmat('Mat/'+level+'F0.mat')['Mat/'+level+'F0'])), JacobiCG(Scipy2PETSc(loadmat('Mat/'+level+'F1.mat')['Mat/'+level+'F1']))]
+
+Fp = Scipy2PETSc(loadmat('Mat/'+level+'Fp.mat')['Mat/'+level+'Fp'])
+F = Multigrid(Scipy2PETSc(loadmat('Mat/'+level+'F.mat')['Mat/'+level+'F']))
+K = Scipy2PETSc(loadmat('Mat/'+level+'K.mat')['Mat/'+level+'K'])
+
+
+
+
+
